4401911aa24c03a8d672de7ad8cfe42f00d46b81
Jelena Mitrovic <jelenamitrovic980@gmail.com>
2020-06-20 17:22:52 +0200

    Implemented Calculator

--- Calculator/src/calculator/Calculator.java
+++ Calculator/src/calculator/Calculator.java
@@ -5,9 +5,11 @@
  */
 package calculator;
 
+import java.util.Scanner;
+
 /**
  *
- * @author A31542R4PN
+ * @author Jelena Mitrović
  */
 public class Calculator {
 
@@ -15,7 +17,49 @@ public class Calculator {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Scanner sc = new Scanner(System.in);
+        Scanner scString = new Scanner(System.in);
+        Scanner scNumber = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        double number1 = sc.nextDouble();
+        System.out.print("Enter the second number: ");
+        double number2 = sc.nextDouble();
+        
+        System.out.print("Enter a operation: (+,-,*,/): ");
+        
+        String operacija = scString.nextLine();
+        
+        switch (operacija) {
+            case "+":
+                System.out.println("Sum is: "+(number1+number2));
+                break;
+            case "-":
+                System.out.println("Difference is: "+(number1-number2));
+                break;
+            case "*":
+                System.out.println("Multiplied is: "+(number1*number2));
+                break;
+            case "/":
+                if(number2==0) {
+                    System.out.println("Zero division is not allowed!");
+                } else {
+                    System.out.println("Quotient is: "+(number1/number2));
+                }
+                break;
+            default:
+                System.out.println("Non exestnt operation!");
+                break;
+        }
     }
     
 }
+
+      
+    
+    
+
+        
+    
+    
+
